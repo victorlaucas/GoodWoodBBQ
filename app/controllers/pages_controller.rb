01d809c6c995ecb5da3185773682104cee657b37
Victor <victorlaucas@gmail.com>
2017-10-21 20:11:32 -0600

    Generated pages controller for static views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def menu
+  end
+
+  def location
+  end
+
+  def about_us
+  end
+
+  def latest_news
+  end
+
+  def faq
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/menu'
+
+  get 'pages/location'
+
+  get 'pages/about_us'
+
+  get 'pages/latest_news'
+
+  get 'pages/faq'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
