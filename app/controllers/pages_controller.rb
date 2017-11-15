f6ed903046e6771597be3057c24731c7a38c1e59
Victor <victorlaucas@gmail.com>
2017-11-14 22:46:58 -0700

    Created university view, controller method, and route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,4 +19,7 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def goodwood_university
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact'
 
+  get 'university', to: 'pages#goodwood_university'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
